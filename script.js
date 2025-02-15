1a39c08d5f474ac346e2bca427b09ff2abc997eb
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2025-02-15 14:19:37 +0530

    Functionality Done Only Design Left.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const popup = document.querySelector('.popup')
 const button = document.querySelector('button')
 const closeIcon = document.querySelector('.close-icon')
+const popup = document.querySelector('.popup-container')
+const overlay = document.querySelector('.overlay')
 
 
 button.addEventListener('click', () => {
-    popup.classList.add('closePopup')
+    popup.classList.add('popup-open')
 })
 
 closeIcon.addEventListener('click', () => {
-    popup.classList.remove('closePopup')
+    popup.classList.remove('popup-open')
+})
+
+overlay.addEventListener('click', () => {
+    popup.classList.remove('popup-open')
 })
